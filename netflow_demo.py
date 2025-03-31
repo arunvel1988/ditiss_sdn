2d878fc4645d0802c32f33f5db0a04a2f084b2aa
Arunvel Arunachalam <arunvel.arunachalam@infosys.com>
2025-03-31 10:56:24 +0000

    Create netflow_demo.py

---
+++ netflow_demo.py
@@ -0,0 +1,21 @@
+from ncclient import manager
+
+# OpenDaylight Credentials
+odl_ip = "192.168.56.101" 
+username = "admin"
+password = "admin"
+
+# Read XML content from file
+with open("port.xml", "r") as file:
+    netconf_payload = file.read()
+
+# Connect to OpenDaylight via NETCONF
+with manager.connect(
+    host=odl_ip,
+    port=830,
+    username=username,
+    password=password,
+    hostkey_verify=False
+) as m:
+    response = m.dispatch(netconf_payload)
+    print(response)
